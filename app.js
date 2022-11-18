b27b4931d939337e0a916db020ec1f12f193fd1a
jamextech1 <okorojameschizaram@gmail.com>
2022-11-18 18:13:20 +0100

    i made the navigation to be responsivel;y working witn no flaws

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const btn = document.querySelector(".hamburger");
-const btnActive = document.querySelector(".hamburger-active");
+const btnActive = document.querySelector(".close");
+const navBar = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li a");
+console.log(navLinks);
+// onclick behaviour
 btn.addEventListener("click", function (e) {
   btn.style.display = "none";
   btnActive.style.display = "block";
+  navBar.style.top = "50%";
+});
+
+//
+btnActive.addEventListener("click", function (e) {
+  btn.style.display = "block";
+  btnActive.style.display = "none";
+  navBar.style.top = "-50%";
+});
+
+navLinks.forEach((navlink) => {
+  navlink.addEventListener("click", function () {
+    btn.style.display = "block";
+    btnActive.style.display = "none";
+    navBar.style.top = "-50%";
+  });
 });
